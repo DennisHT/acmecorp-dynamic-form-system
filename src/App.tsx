a2044f6eeedcf1485a8e27234d476254a3b8d553
DennisHT <dennis.tandio22@gmail.com>
2026-05-30 12:36:29 +0700

    Differentiate between toggle button and submit button

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ export default function App() {
           </>
         )}
         <button
-          className="border"
+          className="border bg-blue-500 text-white px-4 py-2 rounded"
           onClick={handleSubmit(onSubmit, (errs) =>
             console.log("RHF blocked submit:", errs),
           )}
